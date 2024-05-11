61128d82acd4ed9304d064b5b4b69bee6d7a87c3
Antoni Nowalski <antoninowalski@gmail.com>
2024-05-11 11:29:24 +0200

    jwt

--- RestaurantAPI1/Models/Validators/RegisterUserDtoValidator.cs
+++ RestaurantAPI1/Models/Validators/RegisterUserDtoValidator.cs
@@ -4,9 +4,25 @@ namespace RestaurantAPI1.Models.Validators
 {
     public class RegisterUserDtoValidator: AbstractValidator<RegisterUserDto>
     {
-        public RegisterUserDtoValidator()
+        public RegisterUserDtoValidator(RestaurantDbContext dbContext)
         {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .EmailAddress();
 
+            RuleFor(x => x.Password).MinimumLength(8);
+
+            RuleFor(x => x.ConfirmPassword).Equal(e => e.Password);
+
+            RuleFor(x => x.Email)
+                .Custom((value, context) =>
+                {
+                    var emailInUse = dbContext.Users.Any(u => u.Email == value);
+                    if (emailInUse) 
+                    {
+                        context.AddFailure("Email", "Thaat email is taken");
+                    }
+                });
         }
     }
 }
--- RestaurantAPI1/Program.cs
+++ RestaurantAPI1/Program.cs
@@ -1,14 +1,41 @@
 
 
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
 using RestaurantAPI1.Middleware;
+using RestaurantAPI1.Models.Validators;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Logging.ClearProviders();
 builder.Logging.SetMinimumLevel(LogLevel.Trace);
 builder.Logging.AddNLog();
+
 // Add services to the container.
+
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
 
 builder.Services.AddDbContext<RestaurantDbContext>();
 builder.Services.AddScoped<RestaurantSeeder>();
@@ -18,9 +45,10 @@ builder.Services.AddScoped<IDishService, DishService>();
 builder.Services.AddScoped<IAccountService, AccountService>();
 builder.Services.AddScoped<ErrorHandlingMiddleware>();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
+builder.Services.AddScoped<IValidator<RegisterUserDto>, RegisterUserDtoValidator>();
 
 builder.Services.AddSwaggerGen();
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation();
 
 var app = builder.Build();
 
@@ -38,7 +66,7 @@ if (app.Environment.IsDevelopment())
     });
 }
 app.UseMiddleware<ErrorHandlingMiddleware>();
-
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
